fd74a9882570bda8c39837af4b86cfaca4a7696e
evandazar <eval2380@giant.cos.edu>
2020-04-02 18:37:57 -0700

    change the invoice app to replicate the code found in Figure 3-13 in our textbook.

--- src/edu/whccd/evaldez/Main.java
+++ src/edu/whccd/evaldez/Main.java
@@ -1,51 +1,52 @@
 package edu.whccd.evaldez;
 
 import java.util.Scanner;
+import java.text.NumberFormat;
 
 /*
-    The purpose of this application is to replicate the code in Figure 2-17.
+    The purpose of this application is to replicate the code in Figure 3-13.
     author: Evander Valdez
 */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Welcome to the Invoice Total Calculator");
-        System.out.println(); //prints out a blank line
+
+        final double SALES_TAX_PCT = .05;
 
         Scanner sc = new Scanner(System.in);
-
-        //preform invoice calculations until choice isn't equal to "y" or "Y"
         String choice = "y";
-        while (choice.equalsIgnoreCase("y"))
-        {
-            // get the invoice subtotal from the user
-            System.out.print("Enter subtotal: ");
+        while (choice.equalsIgnoreCase("y")) {
+            //get the input from the user
+            System.out.print("Enter subtotal : ");
             double subtotal = sc.nextDouble();
 
-            //calculate the discount amount and total
+            //calculate the results
             double discountPercent = 0.0;
-            if (subtotal >= 200)
-                discountPercent = .2;
-            else if (subtotal>= 100)
+            if (subtotal >= 100)
                 discountPercent = .1;
             else
                 discountPercent = 0.0;
-
             double discountAmount = subtotal * discountPercent;
-            double total = subtotal - discountAmount;
+            double totalBeforeTax = subtotal - discountAmount;
+            double salesTax = totalBeforeTax * SALES_TAX_PCT;
+            double total = totalBeforeTax + salesTax;
 
-            //display the discount amount and total
-            String message = "Discount percent: " + discountPercent + "\n"
-                            + "Discount amount: " + discountAmount + "\n"
-                            + "Invoice total: " + total + "\n";
-            System.out.println(message);
+            //format and display the results
+            NumberFormat currency = NumberFormat.getCurrencyInstance();
+            NumberFormat percent = NumberFormat.getPercentInstance();
+        String message = "Discount percent: " + percent.format(discountPercent) + "\n"
+                + "Discount amount: " + currency.format(discountAmount) + "\n"
+                + "Total before tax: " + currency.format(totalBeforeTax) + "\n"
+                + "Sales tax: " + currency.format(salesTax)+ "\n"
+                + "Invoice total: " + currency.format(total) + "\n";
+        System.out.println(message);
 
-            //see is the use wants to continue
-            System.out.print("Continue? (y/n): ");
-            choice = sc.next();
-            System.out.println();
-        }
-        }
+        //see is the use wants to continue
+        System.out.print("Continue? (y/n): ");
+        choice = sc.next();
+        System.out.println();
     }
+        }
+        }
+
 
